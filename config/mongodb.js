a8293fe3c9eddac874a15d12ff23d2501d6be59a
prashanthsubbiah <prash>
2016-01-10 00:18:07 +0530

    production push variables added

--- config/mongodb.js
+++ config/mongodb.js
@@ -5,5 +5,12 @@ module.exports = {
 		host: 'localhost',
 		port: 27017,
 		database: 'socketApp'
+	},
+	production: {
+		user: 'chirp',
+		pass: 'chirp',
+		host: 'ds039135.mongolab.com',
+		port: 39135,
+		database: 'socketapp'		
 	}
 };
--- config/redis.js
+++ config/redis.js
@@ -3,5 +3,10 @@ module.exports = {
 		auth: '',
 		host: 'localhost',
 		port: 6379
+	},
+	production: {
+		auth: 'chirp',
+		host: 'pub-redis-16129.us-east-1-3.4.ec2.garantiadata.com',
+		port: 16129
 	}
 };
